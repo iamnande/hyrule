d23404175834a154f35e4d602c9a13f0ea87d63d
Nicholas Anderson <nicholas.anderson@konghq.com>
2025-01-20 13:20:37 -0800

    feat(dx): local dev stack

--- mk/run.mk
+++ mk/run.mk
@@ -1,4 +1,23 @@
-ENTRYPOINT := ./cmd/main.go
+ENTRYPOINT    := ./cmd/main.go
+STACK_TIMEOUT := 10
+
+.PHONY: stack-restart
+stack-restart: stack-down stack-up ## run: restart the local environment
+
+.PHONY: stack-up
+stack-up: ## run: start the local environment
+	@echo $(APP_LOG_FMT) "starting local environment"
+	@podman compose up --quiet-pull --build --detach --timeout $(STACK_TIMEOUT)
+
+.PHONY: stack-status
+stack-status: ## run: show the local environment status
+	@echo $(APP_LOG_FMT) "showing local environment status"
+	@podman compose ps
+
+.PHONY: stack-down
+stack-down: ## run: stop the local environment
+	@echo $(APP_LOG_FMT) "stopping local environment"
+	@podman compose down --remove-orphans --volumes --timeout $(STACK_TIMEOUT)
 
 .PHONY: run-signup-api
 run-signup-api: ## run: Admin API
